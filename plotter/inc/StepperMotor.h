79a2242b500beef97b3d80f61ca9e2d3d4cfb34b
tuanngu <nguyenanhtuan96dn@gmail.com>
2017-09-28 11:36:20 +0300

    update gitignore

--- plotter/inc/StepperMotor.h
+++ plotter/inc/StepperMotor.h
@@ -7,11 +7,19 @@
 
 #ifndef STEPPERMOTOR_H_
 #define STEPPERMOTOR_H_
+#include "DigitalIopin.h"
 
 class StepperMotor {
+private:
+	DigitalIoPin xMotor;
+	DigitalIoPin xDir;
+	DigitalIoPin yMotor;
+	DigitalIoPin yDir;
 public:
     StepperMotor();
     virtual ~StepperMotor();
+    void moveX(bool direction, int step);
+    void moveY(bool direction, int step);
 };
 
 #endif /* STEPPERMOTOR_H_ */
