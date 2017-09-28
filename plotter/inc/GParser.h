631bc55f89e15ae3191479aee4de5b17ca079a39
tuanngu <nguyenanhtuan96dn@gmail.com>
2017-09-28 11:32:18 +0300

    implemented G-Code parser

--- plotter/inc/GParser.h
+++ plotter/inc/GParser.h
@@ -8,10 +8,16 @@
 #ifndef GPARSER_H_
 #define GPARSER_H_
 
+#include "Command.h"
+
 class GParser {
 public:
     GParser();
     virtual ~GParser();
+    Command parse(char *instruction, int len);
+
+private:
+    float GParser::findParam(char key, char *instruction, int len);
 };
 
 #endif /* GPARSER_H_ */
--- plotter/src/GParser.cpp
+++ plotter/src/GParser.cpp
@@ -7,6 +7,8 @@
 
 #include <GParser.h>
 
+#include <stdlib.h>
+
 GParser::GParser() {
     // TODO Auto-generated constructor stub
 
@@ -16,3 +18,90 @@ GParser::~GParser() {
     // TODO Auto-generated destructor stub
 }
 
+Command GParser::parse(char *instruction, int len) {
+    Command ret;
+    ret.type = Command::invalid;
+
+    if (instruction[0] == 'G') {
+
+        int code = strtol(instruction + 1, NULL, 10);
+
+        switch (code) {
+        case 1  :
+            ret.type = Command::move;
+            ret.params[0] = findParam('X', instruction, len);
+            ret.params[1] = findParam('Y', instruction, len);
+            ret.params[2] = findParam('A', instruction, len);
+
+            break;
+        case 28 :
+            ret.type = Command::to_origin;
+
+            break;
+        default:
+            break;
+        }
+    } else if (instruction[0] == 'M') {
+
+        int code = strtol(instruction + 1, NULL, 10);
+
+        switch (code) {
+        case 1  :
+            ret.type = Command::pen_position;
+            ret.params[0] = strtol(instruction + 2, NULL, 10);
+
+            break;
+        case 2  :
+            ret.type = Command::pen_setting;
+            ret.params[0] = findParam('U', instruction, len);
+            ret.params[1] = findParam('D', instruction, len);
+
+            break;
+        case 4  :
+            ret.type = Command::laser;
+            ret.params[0] = strtol(instruction + 2, NULL, 10);
+
+            break;
+        case 5  :
+            ret.type = Command::plotter_setting;
+            ret.params[0] = findParam('A', instruction, len);
+            ret.params[1] = findParam('B', instruction, len);
+            ret.params[2] = findParam('H', instruction, len);
+            ret.params[3] = findParam('W', instruction, len);
+            ret.params[4] = findParam('S', instruction, len);
+
+            break;
+        case 10 :
+            ret.type = Command::connected;
+
+            break;
+        case 11:
+            // Ignore
+        default:
+            break;
+        }
+    }
+
+    return ret;
+}
+
+/*
+ * Look for numbers after key and return it
+ * If key is not found, return 0
+ */
+float GParser::findParam(char key, char *instruction, int len) {
+    int keyIndex = -1;
+
+    for (int i = 0; i < len; i++)
+        if (instruction[i] == key) {
+            keyIndex = i;
+        }
+
+    if (keyIndex == -1)
+        return 0;
+    else {
+        float ret = strtof(instruction + keyIndex + 1, NULL);
+
+        return ret;
+    }
+}
