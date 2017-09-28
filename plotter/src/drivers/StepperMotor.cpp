4c12fb9be75233abc0f0a567d1920d3581e45268
minhhungft9 <minhhungft9@gmail.com>
2017-09-28 10:23:05 +0300

    stepper motor move

--- plotter/src/drivers/StepperMotor.cpp
+++ plotter/src/drivers/StepperMotor.cpp
@@ -9,10 +9,32 @@
 
 StepperMotor::StepperMotor() {
     // TODO Auto-generated constructor stub
-
+	this->xMotor = DigitalIoPin(0, 27, DigitalIoPin::output, true);
+	this->xDir = DigitalIoPin(0, 28, DigitalIoPin::output, true);
+	this->yMotor = DigitalIoPin(0, 24, DigitalIoPin::output, true);
+	this->yDir = DigitalIoPin(1,0, DigitalIoPin::output, true);
 }
 
 StepperMotor::~StepperMotor() {
     // TODO Auto-generated destructor stub
 }
 
+void StepperMotor::moveX(bool direction, int step){
+	xDir.write(direction);
+	for(int i = 0; i < step; i++){
+		xMotor.write(true);
+		vTaskDelay(1);
+		xMotor.write(false);
+		vTaskDelay(1);
+	}
+}
+
+void StepperMotor::moveY(bool direction, int step){
+	yDir.write(direction);
+	for(int i = 0; i < step; i++){
+		yMotor.write(true);
+		vTaskDelay(1);
+		yMotor.write(false);
+		vTaskDelay(1);
+	}
+}
