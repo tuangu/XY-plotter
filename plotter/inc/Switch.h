a59f7d89b03cec55f09a949c6bf1ae52baf151d7
tuanngu <nguyenanhtuan96dn@gmail.com>
2017-09-29 14:04:41 +0300

    initialize StepperMotor

---
+++ plotter/inc/Switch.h
@@ -0,0 +1,17 @@
+#ifndef SWITCH_H_
+#define SWITCH_H_
+
+#define limitXMinPort   0
+#define limitXMinPin    29
+#define limitXMaxPort   0
+#define limitXMaxPin    9
+
+#define limitYMinPort   1
+#define limitYMinPin    3
+#define limitYMaxPort   0
+#define limitYMaxPin    0
+
+
+
+
+#endif /* SWITCH_H_ */
